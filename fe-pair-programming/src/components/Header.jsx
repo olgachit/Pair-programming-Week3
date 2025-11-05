a3e5bd0d0ce27e41148c3edf1f9237517de23357
Unai San Segundo <unais@metropolia.fi>
2025-11-05 15:09:45 +0200

    Fix missing css modules in header

--- fe-pair-programming/src/components/Header.jsx
+++ fe-pair-programming/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import logo from '../assets/images/logo.svg'
 import Navbar from './Navbar'
+import styles from '../Navbar.module.css'
 
 function Header() {
   return (
-    <header className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} className='nav-logo' alt='backroads' />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+    <header className={styles.navbar}>
+      <div className={styles['nav-center']}>
+        <div className={styles['nav-header']}>
+          <img src={logo} className={styles['nav-logo']} alt='backroads' />
+          <button type="button" className={styles['nav-toggle']} id="nav-toggle">
             <i className="fas fa-bars" />
           </button>
         </div>
