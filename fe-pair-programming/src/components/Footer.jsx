8c2cae9ad640efdbbcc8695661763bcd967ee7f2
Olga <olga.chitembo@metropolia.fi>
2025-11-05 15:40:46 +0200

    Footer fix

--- fe-pair-programming/src/components/Footer.jsx
+++ fe-pair-programming/src/components/Footer.jsx
@@ -4,10 +4,10 @@ import style from '../Footer.module.css';
 
 function Footer() {
   return (
-    <footer className={style.footer}>
+    <footer className={`section footer ${style.footer}`}>
       <PageLinks parentClass={style['footer-links']} itemClass={style['footer-link']} />
       <SocialLinks parentClass={style['footer-icons']} itemClass={style['footer-icon']} />
-      <p className={style.copyright}>
+      <p className={style['copyright']}>
         copyright © travel tours company <span id="date" />. all rights reserved
       </p>
     </footer>
