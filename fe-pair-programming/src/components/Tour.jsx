86f6a6512bed28dd37de6feccbda980bdafa3215
Olga <olga.chitembo@metropolia.fi>
2025-11-05 14:30:25 +0200

    iteration 4

--- fe-pair-programming/src/components/Tour.jsx
+++ fe-pair-programming/src/components/Tour.jsx
@@ -1,5 +1,29 @@
-function Tour() {
-  return <div>Tour</div>;
+function Tour({ image, date, title, info, location, duration, cost }) {
+  return (
+    <article className="tour-card">
+      <div className="tour-img-container">
+        <img src={image} alt={title} className="tour-img" />
+        <p className="tour-date">{date}</p>
+      </div>
+
+      <div className="tour-info">
+        <div className="tour-title">
+          <h4>{title}</h4>
+        </div>
+        <p>{info}</p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map" />
+            </span>
+            {location}
+          </p>
+          <p>from €{cost}</p>
+          <p>{duration}</p>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default Tour;
--- fe-pair-programming/src/components/Tours.jsx
+++ fe-pair-programming/src/components/Tours.jsx
@@ -1,7 +1,51 @@
-import tour1 from "../assets/images/tour-1.jpeg";
-import tour2 from "../assets/images/tour-2.jpeg";
-import tour3 from "../assets/images/tour-3.jpeg";
-import tour4 from "../assets/images/tour-4.jpeg";
+import Tour from './Tour';
+import tour1 from '../assets/images/tour-1.jpeg';
+import tour2 from '../assets/images/tour-2.jpeg';
+import tour3 from '../assets/images/tour-3.jpeg';
+import tour4 from '../assets/images/tour-4.jpeg';
+
+const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'August 26th, 2027',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'China',
+    duration: '6 days',
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'October 1st, 2027',
+    title: 'Best of Java',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Indonesia',
+    duration: '11 days',
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'September 15th, 2027',
+    title: 'Explore Hong Kong',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Hong Kong',
+    duration: '8 days',
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'December 5th, 2027',
+    title: 'Kenya Highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Kenya',
+    duration: '20 days',
+    cost: 3300,
+  },
+];
 
 function Tours() {
   return (
@@ -12,126 +56,9 @@ function Tours() {
         </h2>
       </div>
       <div className="section-center featured-center">
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img
-              src={tour1}
-              className="tour-img"
-              alt="Tibet Adventure"
-            />
-            <p className="tour-date">august 26th, 2027</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map" />
-                </span>
-                china
-              </p>
-              <p>from €2100</p>
-              <p>6 days</p>
-            </div>
-          </div>
-        </article>
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img
-              src={tour2}
-              className="tour-img"
-              alt="best of java"
-            />
-            <p className="tour-date">october 1th, 2027</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>best of java</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map" />
-                </span>
-                indonesia
-              </p>
-              <p>from €1400</p>
-              <p>11 days</p>
-            </div>
-          </div>
-        </article>
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img
-              src={tour3}
-              className="tour-img"
-              alt="explore hong kong"
-            />
-            <p className="tour-date">september 15th, 2027</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>explore hong kong</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map" />
-                </span>
-                hong kong
-              </p>
-              <p>from €5000</p>
-              <p>8 days</p>
-            </div>
-          </div>
-        </article>
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img
-              src={tour4}
-              className="tour-img"
-              alt="kenya highlights"
-            />
-            <p className="tour-date">december 5th, 2027</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>kenya highlights</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map" />
-                </span>
-                kenya
-              </p>
-              <p>from €3300</p>
-              <p>20 days</p>
-            </div>
-          </div>
-        </article>
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour} />
+        ))}
       </div>
     </section>
   );
