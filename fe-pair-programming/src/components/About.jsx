977282753e5141bba93b29943c6c49e7cbcef3e4
Olga <olga.chitembo@metropolia.fi>
2025-11-05 15:27:46 +0200

    CSS model

--- fe-pair-programming/src/components/About.jsx
+++ fe-pair-programming/src/components/About.jsx
@@ -1,42 +1,40 @@
-import aboutImg from '../assets/images/about.jpeg'
-import React from 'react';
-import styles from '../About.module.css';
+import aboutImg from "../assets/images/about.jpeg";
+import styles from "../About.module.css";
 
 const About = () => {
   return (
     <section className="section" id="about">
-  <div className="section-title">
-    <h2>
-      about <span>us</span>
-    </h2>
-  </div>
-  <div className="section-center about-center">
-    <div className={styles['about-img']}>
-      <img
-        src={aboutImg}
-        className={styles['about-photo']}
-        alt="awesome beach"
-      />
-    </div>
-    <article className={styles['about-info']}>
-      <h3>explore the difference</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-        quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-        unde dolor?
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-        quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-        unde dolor?
-      </p>
-      <a href="#" className={styles.btn}>
-        read more
-      </a>
-    </article>
-  </div>
-</section>
-
+      <div className="section-title">
+        <h2>
+          about <span>us</span>
+        </h2>
+      </div>
+      <div className={`section-center ${styles["about-center"]}`}>
+        <div className={styles["about-img"]}>
+          <img
+            src={aboutImg}
+            className={styles["about-photo"]}
+            alt="awesome beach"
+          />
+        </div>
+        <article className={styles["about-info"]}>
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <a href="#" className={styles.btn}>
+            read more
+          </a>
+        </article>
+      </div>
+    </section>
   );
 };
 export default About;
