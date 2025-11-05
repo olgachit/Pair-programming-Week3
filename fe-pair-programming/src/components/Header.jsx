5302e88ff0c9dcfd098b6b558418ad00a77c3c54
Unai San Segundo <unais@metropolia.fi>
2025-11-05 14:57:33 +0200

    Iteration 7

--- fe-pair-programming/src/components/Header.jsx
+++ fe-pair-programming/src/components/Header.jsx
@@ -1,5 +1,20 @@
+import logo from '../assets/images/logo.svg'
+import Navbar from './Navbar'
+
 function Header() {
-  return <div>Header</div>;
-}
+  return (
+    <header className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+        <Navbar />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
